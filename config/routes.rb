0d69a1a54bdc13b29e47e8824fbbc7f8f35d6d6c
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-25 19:56:00 +1000

    added new message controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chatrooms, only: :show
+  resources :chatrooms, only: :show do 
+    resources :messages, only: :create
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
